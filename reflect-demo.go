e7d491fa750909f73fc951e9fb8d4fadb050f6b7
NgeKaworu <NgeKaworu@163.com>
2020-01-19 10:25:21 +0800

    mongo test

--- reflect-demo.go
+++ reflect-demo.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Test 测试id
 type Test struct {
-	ID primitive.ObjectID `bson:"_id"`
+	ID   *primitive.ObjectID `bson:"_id, omitempty"`
+	Name *string             `bson:"name, omitempty"`
 }
 
 func main() {
-	fmt.Println("hello world")
+	s := "233"
+	data, err := bson.Marshal(&Test{
+		Name: &s,
+	})
+
+	var ret Test
+
+	if err := bson.Unmarshal(data, &ret); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(data, err, ret)
 }
